cab3af7ac0de03c0b5fadba06839d6fc1383d6a6
Alexander Sierra <alexandercsierra@gmail.com>
2020-02-12 13:51:22 -0500

    finished guided lecture

--- server.js
+++ server.js
@@ -1,12 +1,51 @@
 const express = require('express'); // importing a CommonJS module
+const morgan = require("morgan");
+const helmet = require("helmet");
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-server.use(express.json());
+//the three amigas rachel, rita, nancy
+// function logger(req, res, next){
+//   console.log(`${req.method} Request to ${req.originalUrl}`)
+//   console.log('origin', req.originalUrl)
+//   next();
+// }
 
-server.use('/api/hubs', hubsRouter);
+
+// function checkPassword (req, res, next){
+//   //checking if password exists first so there isn't an error changing something to lowercase that doesn't exist
+//   if (req.headers.password && req.headers.password.toLowerCase() === "mellon"){
+//     next();
+//   } else {
+//     console.log("nope wrong password")
+//     res.status(401).json({message: "wrong password"})
+//   }
+// }
+
+function checkPassword (pass){
+  return function (req, res, next){
+    //checking if password exists first so there isn't an error changing something to lowercase that doesn't exist
+    if (req.headers.password && req.headers.password.toLowerCase() === pass){
+      next();
+    } else {
+      console.log("nope wrong password")
+      res.status(401).json({message: "wrong password"})
+    }
+  }
+}
+
+
+//middleware
+server.use(helmet());
+server.use(express.json());
+server.use(morgan("dev"));
+// server.use(logger);
+
+
+
+server.use('/api/hubs', checkPassword('mellon'), hubsRouter);
 
 server.get('/', (req, res) => {
   const nameInsert = (req.name) ? ` ${req.name}` : '';
